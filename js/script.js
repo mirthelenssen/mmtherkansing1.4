a43b59eb740d1e9fffdeae2057224123261e85d1
Mirthe Lenssen <50704463+mirthelenssen@users.noreply.github.com>
2020-08-10 11:27:01 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// var geklikt = false;
+// // var player = videojs('trailer1');
+
+// $("#afspelen").click(function () {
+//     console.log('click');
+//     event.preventDefault();
+
+//     if (geklikt == false) {
+//         $('#trailer1').play();
+//         geklikt = true;
+//         $("#afspelen").css({ 
+
+//         });
+
+//     } else {
+//         $('#trailer1').pause();
+//         geklikt = false;
+//         $("#afspelen").css({
+
+//          });
+//     }
+//     console.log(geklikt);
+// });
+
+$("#afspelen").click(function(){
+    console.log('click');
+})
+
+
+
+$( function() {
+    $( "#tabs" ).tabs();
+  } );
+
+
+     
